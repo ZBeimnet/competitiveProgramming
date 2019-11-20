ced9ef2499329992e357af67880b4c1c44bc9e77
Beimnet Zewdu <zbeimnet@gmail.com>
2019-11-20 16:03:26 +0300

    changes made for reusability

--- Day01/SumTwoNumbers/sumTwoNumber.py
+++ Day01/SumTwoNumbers/sumTwoNumber.py
@@ -4,70 +4,57 @@ Created on Mon Nov 18 19:00:12 2019
 
 @author: Beimnet.Z
 """
-#Global Variables
-number1 = [9, 1, 3, 1, 1, 1, 1, 1, 1, 1]
-number2 = [1, 1, 2, 3, 4, 5, 5, 5, 5, 5]
-number1Length = len(number1)
-number2Length = len(number2)
-length = 0
-difference = 0
+
+#Global Variable
 carry = 0
-result = []
 
-def checkNumLength():
-    global number1
-    global number2
-    global length
-    
-    if(number1Length > number2Length):
-        length = number1Length
-        difference = number1Length - number2Length
-        number1, number2 = makeNumLengthEqual(difference, number2), number1
-    elif(number1Length == number2Length):
-        length = number1Length
-    else:
-        length = number2Length
-        difference = number2Length - number1Length
-        number2, number1 = makeNumLengthEqual(difference, number1), number2
-
-def makeNumLengthEqual(difference, number):
-    num = number
-    for i in range(difference):
-        num.insert(0, 0)
-        
-    return num
-        
-def sumIndexes():
-    global result
-    
-    for numIndex in range(length-1, -1, -1):
-        indexSum = number1[numIndex] + number2[numIndex] + carry
-        result.insert(0, checkSum(numIndex, indexSum))
-
-def checkSum(numIndex, indexSum):
+def calculateSum(firstNumber, secondNumber):
     global carry
+    firstNumberLength = len(firstNumber)
+    secondNumberLength = len(secondNumber)
+    length = 0
+    result = ""
+
+    if(firstNumberLength > secondNumberLength):
+        length = firstNumberLength
+        difference = firstNumberLength - secondNumberLength
+        secondNumber = makeNumberLengthEqual(difference, secondNumber)
+    elif(firstNumberLength == secondNumberLength):
+        length = firstNumberLength
+    else:
+        length = secondNumberLength
+        difference = secondNumberLength - firstNumberLength
+        firstNumber = makeNumberLengthEqual(difference, firstNumber)
+
+    for index in range(length-1, -1, -1):
+        sumAtIndex = int(firstNumber[index]) + int(secondNumber[index]) + carry
+        result = str(checkSumAtIndex(index, sumAtIndex)) + result
+
+    return result
+
+def makeNumberLengthEqual(difference, number):
+    newNumber = number
     
-    strSum = str(indexSum)
-    sumLength = len(strSum)
-    if(sumLength > 1 and numIndex != 0):
-        carry = int(strSum[0])
-        return int(strSum[1])
+    for x in range(difference):
+        newNumber = "0" + newNumber
+        
+    return newNumber
+
+def checkSumAtIndex(index, sumAtIndex):
+    global carry
+
+    if(sumAtIndex > 9 and index != 0):
+        carry = sumAtIndex // 10
+        return sumAtIndex % 10
     else:
         carry = 0
-        return indexSum
-
-def convertListToString(numberList):
-    numberString = ""
-    
-    for number in numberList:
-        numberString += str(number)
-        
-    return numberString
+        return sumAtIndex
 
 def main():    
-    checkNumLength()
-    sumIndexes()
-    print(convertListToString(result))
+    num1 = input("Enter Num1: ")
+    num2 = input("Enter Num2: ")
+    result = calculateSum(num1, num2)
+    print(result)
     
 main()
     
