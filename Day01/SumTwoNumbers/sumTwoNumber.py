b06ff3d884faedde5f2d428ada91924d39e7cf3c
Beimnet Zewdu <zbeimnet@gmail.com>
2019-11-21 11:24:05 +0300

    changed code to conform to python coding style recommendation

--- Day01/SumTwoNumbers/sumTwoNumber.py
+++ Day01/SumTwoNumbers/sumTwoNumber.py
@@ -5,52 +5,52 @@ Created on Mon Nov 18 19:00:12 2019
 @author: Beimnet.Z
 """
 
-#Global Variable
+# Global Variable
 carry = 0
 
-def calculateSum(firstNumber, secondNumber):
+
+def calculate_sum(first_number, second_number):
     global carry
-    firstNumberLength = len(firstNumber)
-    secondNumberLength = len(secondNumber)
-    length = 0
+    first_number_length = len(first_number)
+    second_number_length = len(second_number)
     result = ""
 
-    if(firstNumberLength > secondNumberLength):
-        length = firstNumberLength
-        difference = firstNumberLength - secondNumberLength
-        secondNumber = makeNumberLengthEqual(difference, secondNumber)
-    elif(firstNumberLength == secondNumberLength):
-        length = firstNumberLength
+    if first_number_length > second_number_length:
+        length = first_number_length
+        difference = first_number_length - second_number_length
+        second_number = make_number_length_equal(difference, second_number)
+    elif first_number_length == second_number_length:
+        length = first_number_length
     else:
-        length = secondNumberLength
-        difference = secondNumberLength - firstNumberLength
-        firstNumber = makeNumberLengthEqual(difference, firstNumber)
+        length = second_number_length
+        difference = second_number_length - first_number_length
+        first_number = make_number_length_equal(difference, first_number)
 
-    for index in range(length-1, -1, -1):
-        sumAtIndex = int(firstNumber[index]) + int(secondNumber[index]) + carry
-        result = str(checkSumAtIndex(index, sumAtIndex)) + result
+    for index in range(length - 1, -1, -1):
+        sum_at_index = int(first_number[index]) + int(second_number[index]) + carry
+        result = str(check_sum_at_index(index, sum_at_index)) + result
 
     return result
 
-def makeNumberLengthEqual(difference, number):
-    return "0"*difference + number
-        
-def checkSumAtIndex(index, sumAtIndex):
+
+def make_number_length_equal(difference, number):
+    return "0" * difference + number
+
+
+def check_sum_at_index(index, sum_at_index):
     global carry
+    if sum_at_index > 9 and index != 0:
+        carry = sum_at_index // 10
+        return sum_at_index % 10
+    carry = 0
+    return sum_at_index
 
-    if(sumAtIndex > 9 and index != 0):
-        carry = sumAtIndex // 10
-        return sumAtIndex % 10
-    else:
-        carry = 0
-        return sumAtIndex
 
-def main():    
+def main():
     num1 = input("Enter Num1: ")
     num2 = input("Enter Num2: ")
-    result = calculateSum(num1, num2)
+    result = calculate_sum(num1, num2)
     print("Result: " + result)
-    
-main()
-    
 
+
+main()
