4eb46e6e4257949afa6f094b48119ea37673241b
Beimnet Zewdu <zbeimnet@gmail.com>
2019-12-02 16:21:36 +0300

    PrefixCalculator modified to use the custom stack

--- Week-02/Day08/PrefixCalculator.py
+++ Week-02/Day08/PrefixCalculator.py
@@ -1,24 +1,29 @@
+import sys
+sys.path.insert(1, "D:\CompetitiveProg\Week-02\Day08")
+from Stack import Stack
+
+
 def prefix_calculator(string_input):
     operators = ['+', '-', '*', '/']
     input_list = string_input.split()
     list_length = len(input_list)
 
-    stack = []
+    stack = Stack()
 
     for i in range(list_length-1, -1, -1):
         if input_list[i] not in operators:
-            stack.append(input_list[i])
+            stack.push(input_list[i])
         else:
             operand1 = int(stack.pop())
             operand2 = int(stack.pop())
             if input_list[i] == '*':
-                stack.append(operand1 * operand2)
+                stack.push(operand1 * operand2)
             elif input_list[i] == '/':
-                stack.append(operand1 / operand2)
+                stack.push(operand1 / operand2)
             elif input_list[i] == '+':
-                stack.append(operand1 + operand2)
+                stack.push(operand1 + operand2)
             else:
-                stack.append(operand1 - operand2)
+                stack.push(operand1 - operand2)
 
     return stack.pop()
 
