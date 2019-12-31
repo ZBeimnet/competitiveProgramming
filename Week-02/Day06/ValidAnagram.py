6233854a0f313a62a92faf3c1c9df6862d984a09
Beimnet Zewdu <zbeimnet@gmail.com>
2019-12-31 19:06:26 +0300

    little fix on validAnagram

--- Week-02/Day06/ValidAnagram.py
+++ Week-02/Day06/ValidAnagram.py
@@ -1,13 +1,13 @@
 def is_anagram(string1, string2):
-    string1_count = [0] * 200
-    string2_count = [0] * 200
+    string1_count = [0] * 26
+    string2_count = [0] * 26
     result = True
 
     for char in string1:
-        string1_count[ord(char)] += 1
+        string1_count[ord(char)-97] += 1
 
     for char in string2:
-        string2_count[ord(char)] += 1
+        string2_count[ord(char)-97] += 1
 
     if string1_count != string2_count:
         result = False
