3bb774d738afcaf30e382921099e2cec36100858
Beimnet Zewdu <zbeimnet@gmail.com>
2020-01-01 18:16:43 +0300

    dailyTemprature improved

--- Week-03/Day10/DailyTempratures.py
+++ Week-03/Day10/DailyTempratures.py
@@ -1,14 +1,18 @@
-def dailyTemperatures(T):
-    temp_length = len(T)
-    result = [0] * temp_length
-
-    for i in range(temp_length):
-        for j in range(i + 1, temp_length):
-            if T[j] > T[i]:
-                result[i] = j - i
-                break
-
-    return result
+from typing import List
 
 
-print(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        n = len(T)
+        right_max = T[n-1]
+        res = [0] * n
+        for i in range(n-2, -1, -1):
+            t = T[i]
+            if right_max <= t:
+                right_max = t
+            else:
+                temp = 1
+                while T[i+temp] <= t:
+                    temp += res[i+temp]
+                res[i] = temp
+        return res
