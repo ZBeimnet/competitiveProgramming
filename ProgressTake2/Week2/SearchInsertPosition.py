7bdb8d1c79c712471bb911f2bb835e955731e21b
Beimnet Zewdu <zbeimnet@gmail.com>
2020-09-15 23:52:57 +0300

    updated SearchInsertPosition

--- ProgressTake2/Week2/SearchInsertPosition.py
+++ ProgressTake2/Week2/SearchInsertPosition.py
@@ -2,7 +2,7 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
 
-        while left < right:
+        while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
@@ -11,4 +11,4 @@ class Solution:
             else:
                 left = mid + 1
 
-        return (left, left + 1)[nums[left] < target]
+        return left
