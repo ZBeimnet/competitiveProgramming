4bdc1a800912eb8ff51137f82b2133240fd83fd5
Beimnet Zewdu <zbeimnet@gmail.com>
2020-10-01 07:09:22 +0300

    MinimumDepthOfBinaryTree

---
+++ ProgressTake2/Week4/MinimumDepthOfBinaryTree.py
@@ -0,0 +1,33 @@
+"""
+# run bfs on the tree, state -> [node, depth], root_depth = 1
+    # at each step we check if the current node is a leaf node
+        # if true -> return depth
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        queue = deque([(root, 1)])
+
+        while queue:
+            node, depth = queue.popleft()
+
+            if not node.left and not node.right:
+                return depth
+
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.append((node.right, depth + 1))
+
+
