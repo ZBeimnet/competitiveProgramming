562763cdaf4dbf9068c8cbec12f376d9e082ed33
Beimnet Zewdu <zbeimnet@gmail.com>
2020-10-07 07:36:21 +0300

    MyCalendar

---
+++ ProgressTake2/Week3/MyCalendar.py
@@ -0,0 +1,18 @@
+class MyCalendarTwo:
+    def __init__(self):
+        self.calendar = []
+        self.overlaps = set()
+
+    def book(self, start, end):
+        for i, j in self.overlaps:
+            if start < j and end > i:
+                return False
+        for i, j in self.calendar:
+            if start < j and end > i:
+                self.overlaps.add((max(start, i), min(end, j)))
+        self.calendar.append((start, end))
+        return True
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
