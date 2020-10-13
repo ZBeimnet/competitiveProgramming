95469d7a013525b279fa1048957794cd05f3f1de
Beimnet Zewdu <zbeimnet@gmail.com>
2020-10-13 17:45:31 +0300

    Update PartitionLabels.py

--- ProgressTake2/Week5/PartitionLabels.py
+++ ProgressTake2/Week5/PartitionLabels.py
@@ -13,31 +13,43 @@ class Solution:
         for i, char in enumerate(S):
             end_pos[ord(char) - ord('a')] = i
         
-        partitions = []
-        curr_length = -1
-        start, end = 0, 0
+#         partitions = []
+#         curr_length = -1
+#         start, end = 0, 0
         
-        while end < len(S):
-            if curr_length == -1:
-                if end == end_pos[ord(S[start]) - ord('a')]:
-                    partitions.append(1)
-                    start += 1
-                    end += 1
-                else:
-                    end = end_pos[ord(S[start]) - ord('a')]
-                    curr_length = end - start + 1
-                    start += 1
-            else:
-                if start == end or end == len(S) - 1:
-                    partitions.append(curr_length)
-                    curr_length = -1
-                    start += 1
-                    end += 1
-                elif end_pos[ord(S[start]) - ord('a')] > end:
-                    curr_length += (end_pos[ord(S[start]) - ord('a')] - end)
-                    end = end_pos[ord(S[start]) - ord('a')]
-                    start += 1
-                else:
-                    start += 1
+#         while end < len(S):
+#             if curr_length == -1:
+#                 if end == end_pos[ord(S[start]) - ord('a')]:
+#                     partitions.append(1)
+#                     start += 1
+#                     end += 1
+#                 else:
+#                     end = end_pos[ord(S[start]) - ord('a')]
+#                     curr_length = end - start + 1
+#                     start += 1
+#             else:
+#                 if start == end or end == len(S) - 1:
+#                     partitions.append(curr_length)
+#                     curr_length = -1
+#                     start += 1
+#                     end += 1
+#                 elif end_pos[ord(S[start]) - ord('a')] > end:
+#                     curr_length += (end_pos[ord(S[start]) - ord('a')] - end)
+#                     end = end_pos[ord(S[start]) - ord('a')]
+#                     start += 1
+#                 else:
+#                     start += 1
+		
+		# way cleaner and to the point than my solution (Leetcode's solution)
+        partitions = []
+		start, end = 0, 0
+
+        for i, char in enumerate(S):
+            end = max(end, end_pos[ord(char) - ord('a')])
+            if i == end:
+                partitions.append(i - start + 1)
+                start = i + 1
        
         return partitions
+
+       
