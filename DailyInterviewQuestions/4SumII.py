90a04d1ed42ec721f4437dffc89ac313be6e25a7
Beimnet Zewdu <zbeimnet@gmail.com>
2021-04-25 21:44:21 +0300

    Create 4SumII.py

---
+++ DailyInterviewQuestions/4SumII.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        sum_pairs = defaultdict(int)
+        tuples = 0
+        
+        for i in range(len(A)):
+            for j in range(len(B)):
+                sum_pairs[A[i] + B[j]] += 1
+        
+        for i in range(len(C)):
+            for j in range(len(D)):
+                if -(C[i] + D[j]) in sum_pairs:
+                    tuples += sum_pairs[-(C[i] + D[j])]
+        
+        return tuples
