fd8d745a9eb9e677bad3f896801a3d19f0068b84
Beimnet.Z <zbeimnet@gmail.com>
2021-07-20 15:30:20 +0300

    Update FlattenNestedListIterator

--- Full-timePrep/Week1-Tree/FlattenNestedListIterator.py
+++ Full-timePrep/Week1-Tree/FlattenNestedListIterator.py
@@ -20,6 +20,57 @@
 #        Return None if this NestedInteger holds a single integer
 #        """
 
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.nested_list = nestedList
+        self.stack = [el for el in self.nested_list][::-1]
+        self.next_int = None
+    
+    def next(self) -> int:
+        return self.next_int
+    
+    def hasNext(self) -> bool:
+        return self.find_next_int()
+    
+    def find_next_int(self):
+        while self.stack:
+            curr_el = self.stack.pop()
+            if curr_el.isInteger():
+                self.next_int = curr_el.getInteger()
+                return True
+            else:
+                self.stack.extend([el for el in curr_el.getList()][::-1])
+        return False
+                
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
+
+
+# Approach with python generators
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
         self.nested_list = nestedList
