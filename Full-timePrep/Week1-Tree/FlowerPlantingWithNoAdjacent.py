7fb6545c780eb7184227bc41bdc0d7d3d98563d7
Beimnet.Z <zbeimnet@gmail.com>
2021-07-26 11:15:07 +0300

    Update FlowerPlantingWithNoAdjacent.py

--- Full-timePrep/Week1-Tree/FlowerPlantingWithNoAdjacent.py
+++ Full-timePrep/Week1-Tree/FlowerPlantingWithNoAdjacent.py
@@ -29,3 +29,24 @@ class Solution:
                     new_type = next((x for x in range(1, 5) if x not in excluded_types))
                     answer[neighbour - 1] = new_type
                     queue.append((neighbour, new_type))
+
+
+# Greedy Soln from leetcode discuss
+# https://leetcode.com/problems/flower-planting-with-no-adjacent/discuss/290858/JavaC%2B%2BPython-Greedily-Paint
+from collections import defaultdict, deque
+class Solution:
+    def gardenNoAdj(self, n: int, paths: List[List[int]]) -> List[int]:
+        answer = [0] * n
+        graph = defaultdict(list)
+        
+        # build graph
+        for xi, yi in paths:
+            graph[xi].append(yi)
+            graph[yi].append(xi)
+        
+        # since we have at most 3 neighbour gardens, 
+        # we can greedily choosing 1 type for each garden out of 4 types
+        for i in range(n):
+            answer[i] = ({1, 2, 3, 4} - {answer[x - 1] for x in graph[i + 1]}).pop()
+        
+        return answer
