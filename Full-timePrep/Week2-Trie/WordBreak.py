b869d8e39a4eba8f291b52fbb489049a48a90125
Beimnet.Z <zbeimnet@gmail.com>
2021-08-02 10:12:27 +0300

    Create WordBreak.py

---
+++ Full-timePrep/Week2-Trie/WordBreak.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        return self.dfs(s, set(wordDict), 0, {})
+    
+    def dfs(self, s, word_dict, start, cache):
+        if start in cache:
+            return cache[start]
+        if start == len(s):
+            return True
+        
+        if s[start:] in word_dict:
+            cache[start] = True
+            return True
+        for i in range(start + 1, len(s)):
+            if s[start:i] in word_dict:
+                break_res = self.dfs(s, word_dict, i, cache)
+                if break_res:
+                    cache[start] = True
+                    return True
+        
+        cache[start] = False
+        return False
