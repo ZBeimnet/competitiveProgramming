fb4570472d64ef97483c4cb7bb1f94b4cfbb13c1
Beimnet Zewdu <zbeimnet@gmail.com>
2021-11-10 22:35:59 +0300

    Create FindFirstAndLastPosition.py

---
+++ FullTimeCamp/FindFirstAndLastPosition.py
@@ -0,0 +1,29 @@
+# Problem link - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+import operator
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left_idx = self.find_boundary(nums, target, "LEFT_BOUND") 
+        right_idx = self.find_boundary(nums, target, "RIGHT_BOUND")
+        
+        return [left_idx, right_idx]
+    
+    def find_boundary(self, nums, target, flag):
+        comparator = (operator.gt, operator.ge)[flag == "LEFT_BOUND"]
+        left, right = 0, len(nums) - 1
+        idx = -1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if nums[mid] == target:
+                idx = mid
+                
+            if comparator(nums[mid], target):
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return idx
+    
